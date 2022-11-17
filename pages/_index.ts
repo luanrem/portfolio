595df14e19b93bf3ef23ecb262b665eed98b6dfc
luanrem <luanrem@gmail.com>
2022-11-17 20:22:48 -0300

    Install styledComponents

---
+++ pages/_index.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: rgb(169, 76, 255);
+  background: linear-gradient(
+    90deg,
+    rgba(169, 76, 255, 1) 0%,
+    rgba(0, 0, 0, 1) 29%,
+    rgba(5, 5, 5, 1) 76%,
+    rgba(0, 206, 255, 1) 100%,
+    rgba(5, 5, 5, 1) 100%
+  );
+`;
